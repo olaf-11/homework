46da7114cd193abe44c6292d02ad6c462ea716ee
olaf-11 <olaf.makievsky@gmail.com>
2020-09-30 15:09:49 +0300

    -- v1.0 --

--- homework/lesson08/SpellingNumbers.java
+++ homework/lesson08/SpellingNumbers.java
@@ -26,34 +26,118 @@ class SpellingNumbers {
                              "семьдес€т", "восемьдес€т", "дев€носто"};
     final String[] hundreds = {"", "сто", "двести", "триста", "четыреста", "п€тьсот", "шестьсот",
                                "семьсот", "восемьсот", "дев€тьсот"};
-    final String[][] ranks = {{"тыс€ча", "тыс€чи", "тыс€ч"},
-                              {"миллион", "миллиона", "миллионов"},
-                              {"миллиард", "миллиарда", "миллиардов"}};
+    final String[][] ranks = {{" тыс€ча", " тыс€чи", " тыс€ч"},
+                              {" миллион", " миллиона", " миллионов"},
+                              {" миллиард", " миллиарда", " миллиардов"}};
 
-    //final String[] alternative = {"", "одна", "две"};
+    final String[] thousSuffix = {"", "одна", "две"};// мен€ютс€ окончани€ в цифрах у тыс€ч.
 
-    public String toString(int number){
+    public String toString(long number){
         if (number == 0) return "ноль";
-        return spellHundreds(number);
+        if (number > 999) {
+            return spellBigNumbers(number);
+        }
+        return spellHundreds(number, false);
     }
 
     public String toString(double number){
-        return "";
+        Double num = number;
+        long lPart = num.longValue();// ÷ела€ часть числа
+        num = (number - lPart) * 100;
+        long fPart = num.longValue();// ƒробна€ часть числа (сотые)
+
+        return toString(lPart) + " целых " + toString(fPart) + " сотых.";
     }
 
     public String toWeek(int day){
-        return "";
+        int week = day / 7;
+
+        String str = "" + week;
+
+        if (!((week > 10) && (week < 15))){
+            if (week % 10 == 1){
+                return str += " недел€";
+            } else if (week % 10 > 1 && week % 10 < 5){
+                return str += " недели";
+            }
+        }
+        return str += " недель";
     }
 
     // ƒополнительные методы:
     //
-    private String spellHundreds(int num){
+     private String spellHundreds(long num, boolean isThousands){
+        // до тыс€чи
         if (num < 20) {
-            return firstTwenties[num];
+            if (isThousands && num % 100 != 11 && num % 100 != 12 && ((num % 100) % 10 == 1 || (num % 100) % 10 == 2)) {
+                return thousSuffix[(int) (num % 100) % 10];
+            }
+            return firstTwenties[(int) num];
         }
         if (num < 100){
-            return dozens[num / 10] + " " + firstTwenties[num % 10];
+            if (isThousands && ((num % 100) % 10 == 1 || (num % 100) % 10 == 2)) {
+                return dozens[(int) num / 10] + " " + thousSuffix[(int) (num % 100) % 10];
+            }
+            return dozens[(int) num / 10] + " " + firstTwenties[(int) num % 10];
         }
-        return hundreds[num / 100] + " " + spellHundreds(num % 100);
+        if (num < 1000) {
+            if (isThousands){
+                return hundreds[(int) num / 100] + " " + spellHundreds(num % 100, true);
+            }
+            return hundreds[(int) num / 100] + " " + spellHundreds(num % 100, false);
+        }
+        return "ничего";
+    }
+
+    private String spellBigNumbers(long num){
+        // тыс€чи
+        if (num < 1000000) {
+            long remDec = (num / 1000) % 100;
+            long rem = remDec % 10;
+
+            if (remDec != 11 && rem == 1) {
+                return spellHundreds(num / 1000, true) +
+                       ranks[0][0] + " " + spellHundreds(num % 1000, false);
+            } else if (!(remDec > 10 && remDec < 16) && rem > 1 && rem < 5) {
+                return spellHundreds(num / 1000, true) +
+                       ranks[0][1] + " " + spellHundreds(num % 1000, false);
+            } else {
+                return spellHundreds(num / 1000, true) +
+                       ranks[0][2] + " " + spellHundreds(num % 1000, false);
+            }
+        }
+        // миллионы
+        if (num < 1000_000_000) {
+            long remDec = (num / 1000000) % 100;
+            long rem = remDec % 10;
+
+            if (remDec != 11 && rem == 1) {
+                return spellHundreds(num / 1000000, false) +
+                        ranks[1][0] + " " + spellBigNumbers(num % 1000000);
+            } else if (!(remDec > 10 && remDec < 16) && rem > 1 && rem < 5) {
+                return spellHundreds(num / 1000000, false) +
+                        ranks[1][1] + " " + spellBigNumbers(num % 1000000);
+            } else {
+                return spellHundreds(num / 1000000, false) +
+                        ranks[1][2] + " " + spellBigNumbers(num % 1000000);
+            }
+        }
+        // миллиарды
+        if (num < 1000_000_000_000L) {
+            long remDec = (num / 1000_000_000) % 100;
+            long rem = remDec % 10;
+
+            if (remDec != 11 && rem == 1) {
+                return spellHundreds(num / 1000_000_000, false) +
+                        ranks[2][0] + " " + spellBigNumbers(num % 1000_000_000);
+            } else if (!(remDec > 10 && remDec < 16) && rem > 1 && rem < 5) {
+                return spellHundreds(num / 1000_000_000, false) +
+                        ranks[2][1] + " " + spellBigNumbers(num % 1000_000_000);
+            } else {
+                return spellHundreds(num / 1000_000_000, false) +
+                        ranks[2][2] + " " + spellBigNumbers(num % 1000_000_000);
+            }
+        }
+        return "ничего";
     }
 }
--- homework/lesson08/TestSpellingNumbers.java
+++ homework/lesson08/TestSpellingNumbers.java
@@ -4,10 +4,18 @@ public class TestSpellingNumbers {
     public static void main(String[] args) {
         SpellingNumbers sn = new SpellingNumbers();
 
+        double dNumber = -256783457.2342345;
+        //System.out.println();
+        //for (long i = 897089000; i < 1000_000_000_000L; i += 136733843) {
+        //    System.out.println(i + " : " + sn.toString(i));
+        //}
+        System.out.println(dNumber + " : " + sn.toString(dNumber));
         System.out.println();
-        for (int i = 0; i < 100; i+=1) {
-            System.out.println(i + " :  " + sn.toString(i));
-        }
+
+        // int day = 277;
+        //for (int i = 0; i < 367; i += 7) {
+        //    System.out.println("Дней c начала года: " + i + " -- это " + sn.toWeek(i));
+        //}
         System.out.println();
     }
 }
